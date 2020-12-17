a51d510da057d349644b49b7c617b97c065b04f6
haruyamu <haruya.20020331@icloud.com>
2020-12-17 17:19:19 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe User, type: :model do
       it "全ての値が存在すれば登録できる" do
         expect(@user).to be_valid
       end
-      it 'パスワードが6文字以上で登録できる' do
-        @user.password = 'haruya0331'
-        @user.password_confirmation = "haruya0331"
-        expect(@user).to be_valid
-      end
-      it 'パスワードは、半角英数字混合で入力されている' do
-        @user.password = 'haruya0331'
-        @user.password_confirmation = "haruya0331"
-        expect(@user).to be_valid
-      end
     end
     context "新規登録がうまくいかない時" do
       it "passwordは6字以下だと登録できない" do
@@ -27,11 +17,16 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
+      it "passwordは全角では登録できない" do
+        @user.password = "ああああああ"
+        @user.password_confirmation = "ああああああ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
+      end
       it "パスワード(文字)だけじゃ不可  " do
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
-        # binding.pry
         expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
       end
 
